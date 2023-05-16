69c61e77e8f2496c77a111829964b8c3002ea427
Aaron Olkin <aaronolkin@gmail.com>
2023-05-16 02:17:10 -0400

    New muchado revision

--- export.py
+++ export.py
@@ -8,11 +8,11 @@ lines = []
 
 for i in script:
     text = html.unescape(i.get("text", ""))
-    if i.get("class","") in ["c2", "c7"]:
+    if i.get("class","") in ["c5", "c3"]:
         character = text or "UNKNOWN"
-    elif i.get("class", "") in ["c6", "c10"]:
+    elif i.get("class", "") in ["c7", "c10", "c18", "c16"]:
         lines.append(["STAGE DIRECTIONS", text])
-    elif i.get("class", "") in ["c4", "c17"]:
+    elif i.get("class", "") in ["c8", "c17"]:
         lines.append(["SCENE", text])
     else:
         lines.append([character, text])
